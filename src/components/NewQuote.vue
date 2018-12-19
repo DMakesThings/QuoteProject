d82357bb72c93697c0b8ce8690285794f3bf5f4a
DMakesThings <39001174+DMakesThings@users.noreply.github.com>
2018-12-19 18:53:09 +0700

    Initial commit

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row"> 
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Add New Quote </label>
+                <textarea class="form-control" name="" id="" cols="30" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+                <!-- .prevent stops the default form behavior of submitting form to server (and reloading page) -->
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded',this.quote);
+                this.quote = '';
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
+
